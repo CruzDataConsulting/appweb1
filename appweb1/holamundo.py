b3ed691db2f37bf4e465460cc2d3b84bad918a96
CruzDataConsulting <hdcb13@gmail.com>
2024-05-11 10:04:14 -0600

    se elimino carpeta

--- appweb1/holamundo.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st 
-
-st.title("Mi Primera App con Streamlit") 
-st.header("Información sobre el Conjunto de Datos") 
-st.write(""" Este es un simple ejemplo de una app""")
-st.write("nueva línea")
