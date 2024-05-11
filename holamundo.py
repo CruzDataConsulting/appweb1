9c08f35db3bc3051c1358b3e1990ff5cacace77c
CruzDataConsulting <hdcb13@gmail.com>
2024-05-11 09:55:30 -0600

    segundo prog

---
+++ holamundo.py
@@ -0,0 +1,6 @@
+import streamlit as st 
+
+st.title("Mi Primera App con Streamlit") 
+st.header("Información sobre el Conjunto de Datos") 
+st.write(""" Este es un simple ejemplo de una app""")
+st.write("nueva línea")
