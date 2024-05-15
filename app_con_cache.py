6b09b2f72d571d1a537afe313f0d92162ca028ff
CruzDataConsulting <hdcb13@gmail.com>
2024-05-14 18:13:24 -0600

    con st.cache_data

--- app_con_cache.py
+++ app_con_cache.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 DATA_URL = ('C:/Users/hdcb1/appweb1/dataset.csv')
-@st.cache
+st.cache_data
 def load_data(nrows):
     data=pd.read_csv(DATA_URL, nrows=nrows)
     return data
