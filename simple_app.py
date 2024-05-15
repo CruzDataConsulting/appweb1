c1356e816cc2ae27abc376b2d5e7c45f1de32348
CruzDataConsulting <hdcb13@gmail.com>
2024-05-14 18:23:44 -0600

    new path in csv

--- simple_app.py
+++ simple_app.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import streamlit as st
 
-names_link = 'C:/Users/hdcb1/appweb1/dataset.csv'
+names_link = '/appweb1/dataset.csv'
 names_data = pd.read_csv(names_link)
 
 # Create the title for the web app 
