3f2ddda8c14ae139331a273e24cbebbfec7eaaff
CruzDataConsulting <hdcb13@gmail.com>
2024-05-15 22:06:54 -0600

    sin mejoras

--- filtro_sexo.py
+++ filtro_sexo.py
@@ -10,13 +10,12 @@ def load_data_sex(sex):
     datos_filtrados_sexo=data[data['sex']]
     return datos_filtrados_sexo
 
-sexo_elegido = st.selectbox("Elige Sexo",(load_data_sex['sex'].unique()))
+sexo_elegido = st.selectbox("Elige Sexo", load_data_sex['sex'].unique())
 st.write(f"Selected Option: {sexo_elegido!r}")
-if (sexo_elegido):
-     filtrosexo=load_data_sex(sexo_elegido)
-     cuenta_filas=filtrosexo.shape[0] #numero de filas
-     st.write(f"Coincidencias encontradas: {cuenta_filas}")
 
-#st.dataframe(filtronombre)
-#st.dataframe(filtro_por_rango)
-st.dataframe(filtrosexo)
+filtro_por_sexo = load_data_sex['sexo'] == sexo_elegido
+
+cuenta_filas=filtro_por_sexo.shape[0]
+st.write(f"Total coincidencias {cuenta_filas}")
+
+st.dataframe(filtro_por_sexo)
