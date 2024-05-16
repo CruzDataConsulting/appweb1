617618b161bc97e8ea9236a2f7eb035e3f401102
CruzDataConsulting <hdcb13@gmail.com>
2024-05-15 19:46:46 -0600

    filtro por sexo

--- filtro_sexo.py
+++ filtro_sexo.py
@@ -4,11 +4,11 @@ import pandas as pd
 st.title("Streamlit - Busca rangos")
 
 DATA_URL = ('https://raw.githubusercontent.com/CruzDataConsulting/appweb1/master/dataset.csv')
-st.cache_data
-def load_data_sex(sex):
-    data=pd.read_csv(DATA_URL)
-    datos_filtrados_sexo=data[data['sex'].str.contains(sex)]
-    return datos_filtrados_sexo
+#st.cache_data
+#def load_data_sex(sex):
+#    data=pd.read_csv(DATA_URL)
+#    datos_filtrados_sexo=data[data['sex'].str.contains(sex)]
+#    return datos_filtrados_sexo
 
 sexo_elegido = st.selectbox("Select Sexo",load_data_sex['sex'].unique())
 st.write(f"Selected Option: {sexo_elegido!r}")
