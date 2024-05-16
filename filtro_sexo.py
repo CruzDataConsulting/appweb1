613d52f799547b9266ccdf708f29805f2f89fb23
CruzDataConsulting <hdcb13@gmail.com>
2024-05-15 20:58:21 -0600

    cambio

--- filtro_sexo.py
+++ filtro_sexo.py
@@ -5,12 +5,12 @@ st.title("Streamlit - Busca rangos")
 
 DATA_URL = ('https://raw.githubusercontent.com/CruzDataConsulting/appweb1/master/dataset.csv')
 st.cache_data
-def load_data_sex(sexo):
+def load_data_sex(sex):
     data=pd.read_csv(DATA_URL)
     datos_filtrados_sexo=data[data['sex']]
     return datos_filtrados_sexo
 
-sexo_elegido = st.selectbox("Elige Sexo",load_data_sex['sex'].unique())
+sexo_elegido = st.selectbox("Elige Sexo",(load_data_sex['sex'].unique()))
 st.write(f"Selected Option: {sexo_elegido!r}")
 if (sexo_elegido):
      filtrosexo=load_data_sex(sexo_elegido)
