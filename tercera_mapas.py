e74b9a835fb62b47aa611102dd0d8b833a6f6ec3
CruzDataConsulting <hdcb13@gmail.com>
2024-05-16 21:17:20 -0600

    cambios basicos

--- tercera_mapas.py
+++ tercera_mapas.py
@@ -4,6 +4,11 @@ import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 
+map_data = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], 
+                        columns=['lat', 'lon'])
 
-map_data = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=['lat', 'lon'])
+# titulo de la ap 
+st.title("San francisco Map") 
+st.header("Using Streamlit and Mapbox")
+
 st.map(map_data)
