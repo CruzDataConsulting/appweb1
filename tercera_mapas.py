c72d60a12486eebc73289242d1d7e0478215eb4e
CruzDataConsulting <hdcb13@gmail.com>
2024-05-16 21:48:52 -0600

    mapa de uber

--- tercera_mapas.py
+++ tercera_mapas.py
@@ -15,3 +15,25 @@ st.map(map_data)
 st.markdown("____________")
 
 st.title("Viajes uber")
+
+DATE_COLUMN='date/time'
+DATA_URL=('c:\Users\hdcb1\OneDrive\Documentos\SeniorDataSciense\uber_dataset.csv')
+
+@st.cache
+def load_data(nrows):
+    data=pd.read_csv(DATA_URL,nrows=nrows)
+    lowercase=lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state=st.text('Cargando...')
+data=load_data(1000)
+data_load_state=st.text('Hecho, con cache')
+
+#alguna hora entre 0 y 23
+hour_to_filter=st.slider('Hora', 0, 23, 17)
+filtered_data=data[data[DATE_COLUMN].dt.hour==hour_to_filter]
+
+st.subheader("Mapa de pickups a las %s:00" % hour_to_filter)
+st.map(filtered_data)
