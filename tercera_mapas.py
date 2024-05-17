fbe0ebd1cf500f059fa92252f8b2ac653ef38fc1
CruzDataConsulting <hdcb13@gmail.com>
2024-05-16 21:21:36 -0600

    carga dataset uber

--- tercera_mapas.py
+++ tercera_mapas.py
@@ -4,11 +4,14 @@ import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 
+#crea puntos de gps
 map_data = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], 
                         columns=['lat', 'lon'])
-
-# titulo de la ap 
+# titulo de la app
 st.title("San francisco Map") 
 st.header("Using Streamlit and Mapbox")
-
+#muestra el mapa
 st.map(map_data)
+st.markdown("____________")
+
+st.title("Viajes uber")
