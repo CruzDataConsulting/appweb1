8c3ba43c842ab99835486f8a400bdaf4c69f6aca
CruzDataConsulting <hdcb13@gmail.com>
2024-05-20 18:26:28 -0600

    corrida 2

--- load_csv_1.py
+++ load_csv_1.py
@@ -2,8 +2,16 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import pandas as pd
 
-path="https://raw.githubusercontent.com/CruzDataConsulting/appweb1/master/dataset.csv"
+path="https://raw.githubusercontent.com/CruzDataConsulting/appweb1/master/"#dataset.csv"
 path2="C:/Users/hdcb1/OneDrive/Documentos/SeniorDataSciense/"
 
 cred=credentials.Certificate(path2+"primera-base-5bb2f-firebase-adminsdk-2ch28-b147cbc93b.json")
 firebase_admin.initialize_app(cred)
+
+
+db=firestore.client()
+doc_ref=db.collection(u"names") #names es nombre de la coleccion en firebase
+#importa csv
+df=pd.read_csv(path+"dataset.csv")
+tmp=df.to_dict(orient="recors")
+list(map(lambda x: doc_ref_add(x), tmp))
