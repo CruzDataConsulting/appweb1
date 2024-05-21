e2b31427d8cdebb2aa1590c44390044acdef822f
CruzDataConsulting <hdcb13@gmail.com>
2024-05-20 18:54:02 -0600

    documentado

--- load_csv_1.py
+++ load_csv_1.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 path="https://raw.githubusercontent.com/CruzDataConsulting/appweb1/master/"#dataset.csv"
 path2="C:/Users/hdcb1/OneDrive/Documentos/SeniorDataSciense/"
-
+# para acceder mediante credenciales
 cred=credentials.Certificate(path2+"primera-base-5bb2f-firebase-adminsdk-2ch28-b147cbc93b.json")
 firebase_admin.initialize_app(cred)
 
-
+# Seccion de importacion
 db=firestore.client()
 doc_ref=db.collection(u"names") #names es nombre de la coleccion en firebase
-#importa csv
+# para importar csv
 df=pd.read_csv(path+"demo_names_dataset.csv")
 tmp=df.to_dict(orient="records")
 list(map(lambda x: doc_ref.add(x), tmp))
